cdbd7abf155603a7b234615bbc3fbf0458bc379d
Sai Gunturi <sai@nectarom.com>
2014-03-12 02:09:47 -0500

    Add searching functionality.

--- js/views/recipe.js
+++ js/views/recipe.js
@@ -5,8 +5,12 @@ app.views.Recipe = Backbone.View.extend({
 
     attributes: function () {
         return {
-            class: this.model.get('difficulty')
+            class: 'recipe ' + this.model.get('difficulty')
         };
+    },
+
+    events: {
+        'click .title': 'showDetails'
     },
 
     template: _.template($('#recipe-list-element').html()),
@@ -14,15 +18,26 @@ app.views.Recipe = Backbone.View.extend({
     render: function () {
         this.$el.html(this.template(this.model.toJSON()));
         return this;
+    },
+
+    showDetails: function (e) {
+        $(e.target).siblings('.details').slideToggle('fast');
     }
 });
 
 app.views.Cookbook = Backbone.View.extend({
     el: '#wrapper',
 
+    events: {
+        'keyup #search': 'searchFilter'
+    },
+
     initialize: function (data) {
         this.collection = new app.collections.Cookbook(data);
         this.render();
+
+        this.on('change:searchFilter', this.filterBySearch, this);
+        this.collection.on('reset', this.render, this);
     },
 
     render: function () {
@@ -39,5 +54,19 @@ app.views.Cookbook = Backbone.View.extend({
             model: recipe
         });
         $('#recipes_ul').append(newRecipe.render().el);
+    },
+
+    searchFilter: function (e) {
+        this.searchFilter = e.target.value;
+        this.trigger('change:searchFilter');
+    },
+
+    filterBySearch: function () {
+        this.collection.reset(recipesData, {silent: true});
+        var filterString = this.searchFilter;
+        var filtered = _.filter(this.collection.models, function (item) {
+            return item.get('name').toLowerCase().indexOf(filterString.toLowerCase()) !== -1;
+        });
+        this.collection.reset(filtered);
     }
 });
